f67376f280eb0e2ef634785c0f77af669f892526
CTS <44343964+connessionetech@users.noreply.github.com>
2024-11-22 23:22:50 +0530

    License update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 
 setup(
     name="cdscore",
